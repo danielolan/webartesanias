ffb5187144e0d12d0d9b44838976a0da58f0a793
bspena65 <bspena65@ucatolica.edu.co>
2023-10-04 23:01:40 -0500

    swagger

--- springrestapi/src/main/java/com/ucatolica/springrestapi/SpringrestapiApplication.java
+++ springrestapi/src/main/java/com/ucatolica/springrestapi/SpringrestapiApplication.java
@@ -1,9 +1,12 @@
 package com.ucatolica.springrestapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Ecommerce API Documentation", version = "1.0"))
 public class SpringrestapiApplication {
 
     public static void main(String[] args) {
