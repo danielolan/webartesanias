b8be101b6870398c98e4c08f66e5f5ed59475f69
danielolan <da-olano@hotmail.com>
2023-11-04 23:03:09 -0500

    subo cambios pero no me acrudo que hacen ajajaja

--- front/js/agregaproducto.js
+++ front/js/agregaproducto.js
@@ -1,5 +1,5 @@
 document.getElementById('productForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Previene el comportamiento por defecto del formulario
     
     const formData = new FormData(event.target);
     const data = {};
@@ -14,13 +14,22 @@ document.getElementById('productForm').addEventListener('submit', function(event
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())
+    .then(response => {
+        // Asegúrate de que la respuesta es exitosa
+        if (response.ok) {
+            return response.json();
+        } else {
+            // Maneja la respuesta fallida
+            throw new Error('Something went wrong on api server!');
+        }
+    })
     .then(data => {
         console.log(data);
-       
+        // Redirección a una nueva página, reemplaza 'path/to/your/success-page.html' con tu URL
+        window.location.href = './index.html';
     })
     .catch(error => {
         console.error('Error:', error);
-        
+        // Aquí puedes manejar errores o mostrar alguna alerta al usuario
     });
 });
