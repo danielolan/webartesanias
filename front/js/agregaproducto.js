350a774caf907958c15de7b9e6d80377a13dd2b2
danielolan <da-olano@hotmail.com>
2023-11-23 13:46:43 -0500

    arreglo vacios en agregar producto

--- front/js/agregaproducto.js
+++ front/js/agregaproducto.js
@@ -1,25 +1,40 @@
 document.getElementById('productForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Previene el comportamiento por defecto del formulario
 
-    const formData = new FormData(event.target);
+    let isValid = true;
+    const inputs = document.querySelectorAll('#productForm .form-control');
 
-    // No establezcas 'Content-Type' manualmente para 'multipart/form-data'
-    fetch('http://localhost:8081/api/products/upload', { // Actualiza esta URL
-    method: 'POST',
-    body: formData
-})
-.then(response => {
-    if (!response.ok) {
-        return response.text().then(text => { throw new Error(text) });
+    inputs.forEach(input => {
+        const errorSpan = input.nextElementSibling;
+        if (!input.value.trim()) {
+            input.style.borderColor = "red";
+            errorSpan.style.display = "inline";
+            isValid = false;
+        } else {
+            input.style.borderColor = "";
+            errorSpan.style.display = "none";
+        }
+    });
+
+    if (isValid) {
+        const formData = new FormData(event.target);
+        // No establezcas 'Content-Type' manualmente para 'multipart/form-data'
+        fetch('http://localhost:8081/api/products/upload', { // Actualiza esta URL
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(text => { throw new Error(text) });
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            window.location.href = './index.html';
+        })
+        .catch(error => {
+            console.error('Detailed error:', error);
+        });
     }
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-    window.location.href = './index.html';
-})
-.catch(error => {
-    console.error('Detailed error:', error);
-});
-
 });
