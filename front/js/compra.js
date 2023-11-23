f4cad08139579775cdf80dd09b4f104a27124eba
danielolan <da-olano@hotmail.com>
2023-11-23 14:41:27 -0500

    checkpoin de compras

--- front/js/compra.js
+++ front/js/compra.js
@@ -5,10 +5,18 @@ window.onload = function() {
         let totalCompra = productosElegidos.reduce((total, producto) => total + producto.purchase_total, 0);
         document.getElementById('purchase_total').value = totalCompra;
 
-        let fechaHoy = new Date().toISOString().split('T')[0];
-        document.getElementById('purchase_date').value = fechaHoy;
+        let fechaHoy = new Date();
+        let fechaEnvio = new Date(fechaHoy);
+        fechaEnvio.setDate(fechaHoy.getDate() + 5); // Agrega 5 días a la fecha actual
+
+        let fechaHoyFormato = fechaHoy.toISOString().split('T')[0];
+        let fechaEnvioFormato = fechaEnvio.toISOString().split('T')[0];
+
+        document.getElementById('purchase_date').value = fechaHoyFormato;
+        document.getElementById('shipment_date').value = fechaEnvioFormato; // Establece la fecha de envío en el formulario
     }
 };
+
 document.querySelector('.button-enviar').addEventListener('click', function(e) {
     e.preventDefault();
 
@@ -20,6 +28,8 @@ document.querySelector('.button-enviar').addEventListener('click', function(e) {
         })).then(() => {
             // Todas las actualizaciones de inventario han sido exitosas
             console.log("Todas las actualizaciones de inventario han sido exitosas");
+            mostrarNotificacion('Su compra se ha realizado con Exxito');
+            
             // Aquí llamar a una función para eliminar todos los elementos de la tabla purchase
             eliminarTablaPurchase();
         }).catch(error => {
@@ -79,5 +89,35 @@ function initMap() {
       handleLocationError(false, map, map.getCenter());
     }
   }
+  function mostrarNotificacion(mensaje) {
+    // Crear el div de la notificación
+    const notificacion = document.createElement('div');
+    notificacion.className = 'alert alert-success alert-dismissible fade show';
+    notificacion.role = 'alert';
+    notificacion.style.position = 'fixed';
+    notificacion.style.top = '20px';
+    notificacion.style.right = '20px';
+    notificacion.style.zIndex = '1000';
+    notificacion.textContent = mensaje;
+
+    // Botón para cerrar la notificación
+    const botonCerrar = document.createElement('button');
+    botonCerrar.type = 'button';
+    botonCerrar.className = 'close';
+    botonCerrar.setAttribute('data-dismiss', 'alert');
+    botonCerrar.innerHTML = '&times;';
+
+    notificacion.appendChild(botonCerrar);
+
+    // Añadir la notificación al body
+    document.body.appendChild(notificacion);
+
+    // Tiempo para que la notificación desaparezca automáticamente
+    setTimeout(() => {
+        $(notificacion).alert('close');
+        window.location.href = './index.html'; // Redireccionar después de que la notificación se haya cerrado
+    }, 3000); // Ajusta este tiempo según sea necesario
+}
+
   
 
